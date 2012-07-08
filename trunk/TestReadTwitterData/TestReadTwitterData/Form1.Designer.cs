4584856931b3f1627004f0b33281649d2de20c5b
tdquang7 <tdquang7@5ab07d07-27a1-66a8-374e-d3943f640555>
2012-07-08 03:44:50 +0000

    Đang lỗi nodes từ csdl có một số không có cạnh. Thêm project tạo input nữa đề phòng bất trắc

---
+++ trunk/TestReadTwitterData/TestReadTwitterData/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TestReadTwitterData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.progressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(84, 327);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(15, 19);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(339, 302);
+            this.txtContent.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.progressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 363);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(361, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(191, 327);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 385);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Triangle counting";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ trunk/TestReadTwitterData/TestReadTwitterData/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TestReadTwitterData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundGenerateParMetisInput);
+            backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker1_ProgressChanged);
+            backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker1_RunWorkerCompleted);
+        }
+
+        void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnGenerate.Enabled = true;
+            btnCancel.Enabled = false;
+            MessageBox.Show("Completed");
+        }
+
+        void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        void backgroundGenerateParMetisInput(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            if (worker.CancellationPending)
+                e.Cancel = true;
+            else
+            {
+                string inputFile = @"E:\Lab\Triangles data\soc-LiveJournal1.txt";
+                string xadjPath = @"E:\Lab\Triangles data\xadj.txt";
+                string adjncyPath = @"E:\Lab\Triangles data\ajdncy.txt";
+                string infoPath = @"E:\Lab\Triangles data\info.txt";
+
+                StreamReader reader = new StreamReader(inputFile);
+                StreamWriter xadjWriter = new StreamWriter(xadjPath);
+                StreamWriter adjncyWriter = new StreamWriter(adjncyPath);
+                StreamWriter infoWriter = new StreamWriter(infoPath);
+
+                // Header
+                string line;
+
+                // First two line keep for debug info
+                line = reader.ReadLine();
+                line = reader.ReadLine();
+
+                // Third line show how many nodes and edges
+                line = reader.ReadLine(); // Formar: "# Nodes: x Edges: y". Need x and y
+
+                string SPACE = " ";
+                string TAB = "\t";
+
+                // Extract x and y                
+                string[] parts = line.Split(new string[] { SPACE }, StringSplitOptions.None);
+                int nodesNumber = int.Parse(parts[2]);
+                int edgesNumber = int.Parse(parts[4]);
+                
+
+                line = reader.ReadLine(); // Forth line just the table header
+                                
+                string lastId = "";
+                int count = 0;
+
+                for (int i = 0; i < edgesNumber; i++)
+                {
+                    line = reader.ReadLine(); // Format: SourceId \t DestId
+                    parts = line.Split(new string[] { TAB }, StringSplitOptions.None);
+                    string id = parts[0];
+                    string destId = parts[1];
+
+                    if (id != lastId) // Change id occurs
+                    {
+                        lastId = id;
+
+                        xadjWriter.Write(i + " "); // Not id but i, yes!
+                        count++;
+                    }
+
+                    adjncyWriter.Write(destId + " ");  
+                }
+
+                infoWriter.WriteLine(count);
+                infoWriter.WriteLine(edgesNumber);
+               
+                reader.Close();
+                xadjWriter.Close();
+                adjncyWriter.Close();
+                infoWriter.Close();
+
+                reader.Dispose();
+                xadjWriter.Dispose();
+                adjncyWriter.Dispose();
+                infoWriter.Dispose();
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            btnGenerate.Enabled = false;
+            btnCancel.Enabled = true;
+
+            backgroundWorker1.RunWorkerAsync();            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+                backgroundWorker1.CancelAsync();
+        }
+
+    }
+}
