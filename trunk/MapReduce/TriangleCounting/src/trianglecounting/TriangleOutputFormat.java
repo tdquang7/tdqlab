478a1a5d3755ede76e68dd5ec3695240691664e9
tdquang7 <tdquang7@5ab07d07-27a1-66a8-374e-d3943f640555>
2012-07-27 07:16:44 +0000

    Finish reader and writer

--- trunk/MapReduce/TriangleCounting/src/trianglecounting/TriangleOutputFormat.java
+++ trunk/MapReduce/TriangleCounting/src/trianglecounting/TriangleOutputFormat.java
@@ -4,10 +4,34 @@
  */
 package trianglecounting;
 
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.RecordWriter;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputCommitter;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 /**
  *
  * @author tdquang
  */
-public class TriangleOutputFormat {
+public class TriangleOutputFormat extends FileOutputFormat<Text, NodeInfo>{
+
+    @Override
+    public RecordWriter<Text, NodeInfo> getRecordWriter(TaskAttemptContext tac) throws IOException, InterruptedException {
+        Configuration conf = tac.getConfiguration();
+                        
+        FileOutputCommitter committer = (FileOutputCommitter) getOutputCommitter(tac);
+        Path path = new Path(committer.getWorkPath(), getUniqueFile(tac, "part", ""));
+        
+        FileSystem fs = path.getFileSystem (conf);
+        FSDataOutputStream fileOut = fs.create (path, false);
+        
+        return new TriangleRecordWriter(fileOut);
+    }
     
 }
